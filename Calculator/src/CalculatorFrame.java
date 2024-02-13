77dcc3698fc00b5658f30560901df5e7eae1c089
+ <+>
2024-02-13 17:53:10 +0300

    🚧 set the frame for the calculator

--- Calculator/src/CalculatorFrame.java
+++ Calculator/src/CalculatorFrame.java
@@ -1,8 +1,19 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 public class CalculatorFrame extends Abstract {
-    
+
+    JFrame Frame;
     public CalculatorFrame(String name, int width, int height) {
         super(name, width, height);
+
+      Frame=new JFrame(this.name);
+      Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Frame.setSize(this.width,this.height);
+      Frame.setLayout(null);
+      Frame.setVisible(true);
     }
+    
 
     // Implement abstract methods
     @Override
--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -1,3 +1,7 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        CalculatorFrame frame=new CalculatorFrame("Calculator",1200,800);
+
+
+    }
 }

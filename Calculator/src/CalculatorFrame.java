64e916fb3eacffcab7482fe9623f6413ea0dab1a
+ <+>
2024-02-14 17:13:45 +0300

    💥 introduced some scientific breaking features

--- Calculator/src/CalculatorFrame.java
+++ Calculator/src/CalculatorFrame.java
@@ -3,22 +3,33 @@ import java.awt.*;
 
 public class CalculatorFrame extends Abstract {
     InputSection inputSection;
+    ScientificSection scientificSection;
+
     JFrame frame;
 
     public CalculatorFrame(String name, int width, int height) {
         super(name, width, height);
         //Frame section
         frame = new JFrame(this.name);
-        frame.setLayout(null); // Use null layout to manually set component positions
+        // To manually set components position
+        frame.setLayout(null); 
+        //To close the program when cross is clicked
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(this.width, this.height);
 
         // Create input section
         inputSection = new InputSection();
-        inputSection.setBounds(50,0,1200, 50); // Example bounds for input section
+        inputSection.setBounds(50,0,1200, 50); 
+        frame.add(inputSection);
+
+        // Scientific section
+        scientificSection= new ScientificSection();
+        scientificSection.setBounds(50, 100, 300, 300);
+        frame.add(scientificSection);
+    
        
 
-        frame.add(inputSection);
+       
 
         //Digits section
 
--- Calculator/src/ScientificSection.java
+++ Calculator/src/ScientificSection.java
@@ -1,3 +1,39 @@
-public class ScientificSection {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class ScientificSection extends JPanel implements ActionListener {
+    JPanel panel;
+    // JButton[] scientificButtons = new JButton[]{
+    //     new JButton("("), new JButton(")"), new JButton("mc"), new JButton("m+"), new JButton("m-"),
+    //     new JButton("mr"), new JButton("2nd"), new JButton("x²"), new JButton("x³"), new JButton("X^y"),
+    //     new JButton("e^x"), new JButton("10^x"), new JButton("1/x"), new JButton("√"), new JButton("3√"),
+    //     new JButton("LN(x)"), new JButton("log"), new JButton("x!"), new JButton("sin"), new JButton("cos"),
+    //     new JButton("tan"), new JButton("e"), new JButton("EE"), new JButton("Rad"), new JButton("sinh"),
+    //     new JButton("cosh"), new JButton("tanh"), new JButton("\u03C0"), new JButton("rand")
+    // };
+
+    public ScientificSection() {
     
+        // for (JButton button : scientificButtons) {
+        //     button.addActionListener(this);
+        //     button.setFont(new Font("Poppins",Font.PLAIN,30));
+        //     button.setFocusable(false);
+        // }
+
+        panel=new JPanel();
+        // panel.setLayout(new GridLayout(4,4,10,10));
+        panel.setBackground(Color.GRAY);
+
+        add(panel);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Handle button click events here
+    }
 }
